6529bed3b9d408cbb166822115cbe42d38581a1e
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-21 20:58:21 -0300

    Implementacion de todas las funciones de la clase Diccionario

--- ejercitacion_templates_corregida/src/Diccionario.hpp
+++ ejercitacion_templates_corregida/src/Diccionario.hpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #ifndef __DICCIONARIO_HPP
 #define __DICCIONARIO_HPP
 
@@ -8,6 +10,11 @@ public:
     void definir(Clave k, Valor v);
     bool def(Clave k) const;
     Valor obtener(Clave k) const;
+    unsigned int encontrarMinPos(std::vector<Clave> &keys, unsigned int desde, unsigned int hasta) const;
+    void intercambiar(std::vector<Clave> &keys, unsigned int indice, unsigned int minimo) const;
+    void selectionSort(std::vector<Clave> &keys) const;
+    std::vector<Clave> claves() const;           // Ejercicio 8
+
 private:
 
     struct Asociacion {
@@ -57,6 +64,46 @@ Valor Diccionario<Clave,Valor>::obtener(Clave k) const {
     assert(false);
 }
 
-#endif /*__DICCIONARIO_HPP*/
+/* Ejercicio 8 */
+
+template<class Clave, class Valor>
+unsigned int Diccionario<Clave,Valor>::encontrarMinPos(std::vector<Clave> &keys, unsigned int desde, unsigned int hasta) const {
+    unsigned int min = desde;
+    for(unsigned int i = desde + 1; i < hasta; i++){
+        if(keys[i] <= keys[min]){
+            min = i;
+        }
+    }
+    return min;
+}
+
+template<class Clave, class Valor>
+void Diccionario<Clave,Valor>::intercambiar(std::vector<Clave> &keys, unsigned int indice, unsigned int minimo) const {
+    Clave tmp = keys[indice];
+    keys[indice] = keys[minimo];
+    keys[minimo] = tmp;
+}
+
+template<class Clave, class Valor>
+void Diccionario<Clave,Valor>::selectionSort(std::vector<Clave> &keys) const {
+    for(unsigned int i = 0; i < keys.size(); i++) {
+        unsigned int minPos = encontrarMinPos(keys, i, keys.size());
+        intercambiar(keys, i, minPos);
+    }
+
+}
+
+template<class Clave, class Valor>
+std::vector<Clave> Diccionario<Clave,Valor>::claves() const {
+    std::vector<Clave> v1;
+    for(unsigned int i = 0; i < _asociaciones.size(); i++){
+        v1.push_back(_asociaciones[i].clave);
+    }
+    selectionSort(v1);
+    return v1;
+
+}
+
+#endif /*__DICCIONARIO_HPP */
 
 
